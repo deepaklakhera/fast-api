2fc75b11647b06dccfb71f2cf21bc94706db3b53
deepaklakhera <deepaklakhera24hr@gmail.com>
2025-07-26 13:22:34 +0530

    Added github action

--- app/database.py
+++ app/database.py
@@ -1,11 +1,13 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 import os
+from dotenv import load_dotenv
 
+load_dotenv()
 # Get DB URL from env or fallback
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:postgres@db:5432/fastapi_db")
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(DATABASE_URL,connect_args={"sslmode": "require"})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 def get_db():
